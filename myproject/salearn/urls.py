6a7f35ae8352f245c2bff00cafdc47f9dc69c265
unknown <thabangmotswenyane511@gmail.com>
2024-02-16 12:10:54 -0800

    created a new view and created a template both for loging in

--- myproject/salearn/urls.py
+++ myproject/salearn/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.hello_world, name='hello_world'),
+    path('login', views.login_view, name='login_page')
     # path('nav', views.navigation, name="nav"),
 ]
--- myproject/salearn/views.py
+++ myproject/salearn/views.py
@@ -1,12 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
 from django.http import HttpResponse
 
 
 
 def hello_world(request):
-    
     context = {'name':'Thabang Motswenyane'}
     return render(request, 'index.html', context)
+    
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                # Handle successful login (e.g., redirect to dashboard)
+                return redirect('dashboard')
+    else:
+        form = LoginForm()
+    return render(request, 'login.html', {'form': form})
 
 def navigation(request):
     context = {'name':'Thabang Motswenyane'}
