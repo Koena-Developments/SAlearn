297a81e4b08d5abac0f7f420e331652baaf9f4f3
Koena-Development <thabangmotswenyane511@gmail.com>
2024-03-08 05:22:33 -0800

    updated models for Video and comments

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -134,7 +134,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+# Define the directory where static files will be collected during deployment
+STATIC_ROOT = BASE_DIR / 'static'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- myproject/salearn/models.py
+++ myproject/salearn/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -6,4 +7,34 @@ class UserProfile(models.Model):
     username = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=100)
+    phonenumber = models.CharField(max_length=10)
 
+
+class LecturePortal(models.Model):
+    """
+    creating a lecture model to handle all the attributes that the lecture's view contains
+    """
+
+    pass
+
+class Video(models.Model):
+    title = models.charfield(max_length=100)
+    description = models.TextField()
+    length = models.DurationField()
+    video_file = models.FileField(upload_to="videos/")
+    uploader = models.ForeignKey(User, on_delete=models.CASCADE)
+    upload_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    video = models.ForeignKey(Video, on_delete=models.CASCADE,related_name='comments')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.user.username}'s comment on {self.video.title}"
+    
